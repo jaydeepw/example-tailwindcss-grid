1901e0c72c041360a3fcc87219f920c5ddb80c08
Jaydeep <jaydeep.w@gmail.com>
2024-11-02 12:23:47 +0530

    First cut

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { SAMPLE_IMAGES } from "@/utils/constants";
 import localFont from "next/font/local";
 import Image from "next/image";
 
@@ -16,37 +17,32 @@ const geistMono = localFont({
 export default function Home() {
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-20 sm:p-16 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="bg-[#FF0000]">
-          Hello
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <div class="grid grid-cols-3 gap-4">
-              <div className="bg-red-300 p-6 border-2 rounded">Grid Item 1</div>
-              <div className="bg-blue-300 p-6 border-2 rounded">
-                Grid Item 2
-              </div>
-              <div className="bg-green-300 p-6 border-2 rounded">
-                Grid Item 3
-              </div>
-              <div className="bg-yellow-300 p-6 border-2 rounded">
-                Grid Item 4
-              </div>
-              <div className="bg-purple-300 p-6 border-2 rounded">
-                Grid Item 5
-              </div>
-              <div className="bg-orange-300 p-6 border-2 rounded">
-                Grid Item 6
-              </div>
-            </div>
+        <div class="container mx-auto px-5 py-2">
+          <h1 class="text-6xl font-bold text-center mb-16 text-gray-200">
+            Tailwind CSS Image Grid
+          </h1>
+          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            {SAMPLE_IMAGES.map((image) => {
+              return (
+                <div class="overflow-hidden rounded-lg shadow-lg">
+                  <img
+                    src={image}
+                    alt="Image 1"
+                    class="w-full h-60 object-cover hover:scale-105 transition-transform duration-300"
+                  />
+                </div>
+              );
+            })}
           </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex gap-6 mt-20 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          href="https://toofancoder.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -73,7 +69,6 @@ export default function Home() {
             width={16}
             height={16}
           />
-          {/* TODO: */}
           Visit Blog Post →
         </a>
       </footer>
